5bb42f16d3e79dbbd2d472e2b86b1b0b7e60018f
Ashok Babu <pashokbabu.38@gmail.com>
2026-09-10 15:58:44 +0530

    implement proxy

--- Frontend_page/vite.config.js
+++ Frontend_page/vite.config.js
@@ -14,4 +14,29 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5001',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/superadmin': {
+        target: 'http://localhost:5001',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/uploads': {
+        target: 'http://localhost:5001',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/health': {
+        target: 'http://localhost:5001',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
